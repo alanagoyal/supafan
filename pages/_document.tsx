111f073dd67aa2eefcb2091d8765f16d2cfc2c49
Alana Anderson <alanaanderson@MacBook-Air-4.local>
2023-03-06 13:38:35 -0800

    update metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon-32x32.png" />
+        <meta name="description" content="Love Supabase? Tell us about it." />
+        <meta property="og:site_name" content="supafan.dev" />
+        <meta
+          property="og:description"
+          content="Love Supabase? Tell us about it."
+        />
+        <meta property="og:title" content="Supafan" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Supafan" />
+        <meta
+          name="twitter:description"
+          content="Love Supabase? Tell us about it."
+        />
+        <meta property="og:image" content="https://supafan.vercel.app/api/og" />
+        <meta
+          name="twitter:image"
+          content="https://supafan.vercel.app/api/og"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
