e422c08e2bd12ede9efd7645f674d47113f3a41e
Alana Anderson <alanaanderson@MacBook-Air-4.local>
2023-03-06 11:18:54 -0800

    remove bg gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,9 +11,7 @@ function Page({
   messages: Database["public"]["Tables"]["messages"]["Row"];
 }) {
   return (
-    <div className="absolute w-full h-screen">
-      <div className="absolute w-[600px] h-[400px] top-[-200px] left-[-200px] bg-green-400 rounded-full blur-[100px] opacity-20 dark:opacity-10" />
-      <div className="absolute w-[800px] h-[500px] bottom-[-200px] right-0 bg-green-200 rounded-full blur-[100px] opacity-20 dark:opacity-10" />
+    <div>
       <Message />
       <Display />
       <FrigadeChecklist
