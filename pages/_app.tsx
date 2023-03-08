dc7c91dbbe7ccbab498152f55d2642ab68d64605
Alana Anderson <alanaanderson@MacBook-Air-4.local>
2023-03-08 12:35:42 -0800

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { FrigadeProvider } from "@frigade/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <FrigadeProvider publicApiKey={process.env.FRIGADE_API_KEY!}>
+    <>
       <Component {...pageProps} />
-    </FrigadeProvider>
+      <Analytics />
+    </>
   );
 }
