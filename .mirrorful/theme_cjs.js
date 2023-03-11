d2a37c00956d84413aa8daabbd81eb3ee9307a28
alexdanilowicz <alex.danilowicz@gmail.com>
2023-03-11 12:33:36 -0800

    Upgrade Mirrorful to 4.0.0

---
+++ .mirrorful/theme_cjs.js
@@ -0,0 +1,20 @@
+exports.Tokens = {
+  colors: {
+    "supabase-green": {
+      400: "#3dc688",
+      500: "#2b825b",
+      base: "#2b825b",
+    },
+    "supabase-gray": {
+      500: "#3b3b3b",
+      800: "#1c1c1c",
+      900: "#000000",
+      base: "#3b3b3b",
+    },
+  },
+  fontSizes: {
+    sm: "1rem",
+    md: "1.2rem",
+    lg: "1.4rem",
+  },
+};
